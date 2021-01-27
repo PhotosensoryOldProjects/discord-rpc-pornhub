46a4673e8713d2aad1ace5d35ae92536d5a2a058
photosensory <realsolarstuds@gmail.com>
2021-01-27 00:50:42 +0000

    more stuff i forgot to push

--- index.js
+++ index.js
@@ -3,12 +3,41 @@ require("dotenv").config();
 const CLIENT_ID = process.env.CLIENT_ID
 
 const RPC = require("discord-rpc");
-const PornHub = require("pornhub.js");
+const Utils = require("./utils.js");
 
 const client = new RPC.Client({ transport: "ipc" });
-const pornhub = new PornHub();
+const utils = new Utils();
 
 let videoLength = 1.8e+6;
+
+async function setActivity() {
+    const video = await utils.pickVideo();
+    if (video !== undefined) {
+        videoLength = utils.javascriptProsWouldLaughAtMeRightNow(video.duration);
+        client.setActivity({
+            details: `Watching: ${video.title}`,
+            startTimestamp: Date.now(),
+            largeImageKey: process.env.IMAGE_KEY,
+            instance: false
+        });
+    } else {
+        videoLength = 1.8e+6;
+        client.setActivity({
+            details: `Watching: Nothing`,
+            startTimestamp: Date.now(),
+            largeImageKey: process.env.IMAGE_KEY,
+            instance: false
+        });
+    }
+}
+
+// Ready Listener
+client.on("ready", async () => {
+    await setActivity();
+    setInterval(async () => {
+        await setActivity();
+    }, videoLength);
+});
 
 // Login
 client.login({ clientId: CLIENT_ID });
---
+++ utils.js
@@ -0,0 +1,33 @@
+const PornHub = require("pornhub.js");
+const pornhub = new PornHub();
+
+const config = require("./config.js");
+
+module.exports = class Utils {
+    constructor() {};
+
+    pickRandomFrom(arr) {
+        return arr[Math.floor(Math.random() * arr.length)];
+    }
+
+    async pickVideo() {
+        const word = this.pickRandomFrom(config.searchTerms);
+        let videoArray = undefined;
+
+        await pornhub.search("Video", word).then((response) => {
+            videoArray = this.pickRandomFrom(response.data);
+        }).catch(() => {
+            videoArray = undefined; 
+        });
+
+        return videoArray
+    }
+
+    javascriptProsWouldLaughAtMeRightNow(duration) {
+        // theres probably a better way of doing this.
+        // wag1 to all those javascript pros out there.
+        const minutes = parseInt(duration.substring(0, duration.indexOf(":"))) * 60000;
+        const seconds = parseInt(duration.split(":")[1]) * 1000;
+        return minutes + seconds;
+    }
+}
